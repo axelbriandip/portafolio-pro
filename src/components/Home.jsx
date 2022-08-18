8b44696d092a010f8d94479d9bc08b0b68b221a5
axelbriandip <axelbriandip.rg@gmail.com>
2022-08-18 01:49:15 -0300

    tooltip added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,9 +7,18 @@ const Home = () => {
             <div className="circles">
                 <div className="photo animate__animated animate__backInDown"></div>
                 <div className="social-media animate__animated animate__backInLeft">
-                    <div className="item-sm linkedin"><i className="fa-brands fa-linkedin-in"></i></div>
-                    <div className="item-sm cv"><i className="fa-solid fa-file"></i></div>
-                    <div className="item-sm github"><i className="fa-brands fa-github"></i></div>
+                    <div className="item-sm linkedin" id='tooltip'>
+                        <i className="fa-brands fa-linkedin-in"></i>
+                        <span id='tooltipText'>Linkedin</span>
+                    </div>
+                    <div className="item-sm cv" id='tooltip'>
+                        <i className="fa-solid fa-file"></i>
+                        <span id='tooltipText'>Curriculum Vitae</span>
+                    </div>
+                    <div className="item-sm github" id='tooltip'>
+                        <i className="fa-brands fa-github"></i>
+                        <span id='tooltipText'>Github</span>
+                    </div>
                 </div>
             </div>
             {/* text */}
@@ -17,9 +26,7 @@ const Home = () => {
                 <p className='name'><i className="fa-solid fa-hand animate__animated animate__wobble animate__delay-1s"></i> Soy <span>Axel Brian Dip</span>,</p>
                 <h1 className='title'>DESARROLLADOR <br /> FRONT-END.</h1>
                 <p className='description'>Me dedico al diseño, creación, realización y mantenimiento <br /> de páginas y/o aplicaciones web.</p>
-                <button>
-                    Contactame
-                </button>
+                <button>Contactame</button>
             </div>
         </section>
     );
