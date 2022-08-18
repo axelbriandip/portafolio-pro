3cc5dca918654394d4a7f827ab88320c34ee6f52
axelbriandip <axelbriandip.rg@gmail.com>
2022-08-18 03:29:36 -0300

    footer of home done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
     return (
         <section className='container-home'>
             {/* circles */}
-            <div className="circles col-5 col-s-12">
+            <article className="circles col-5 col-s-12">
                 <div className="photo animate__animated animate__backInDown"></div>
                 <div className="social-media animate__animated animate__backInLeft">
                     <div className="item-sm linkedin" id='tooltip'>
@@ -20,14 +20,18 @@ const Home = () => {
                         <span id='tooltipText'>Visitar Github</span>
                     </div>
                 </div>
-            </div>
+            </article>
             {/* text */}
-            <div className="text col-5 col-s-12 animate__animated animate__backInRight">
+            <article className="text col-5 col-s-12 animate__animated animate__backInRight">
                 <p className='name'><i className="fa-solid fa-hand animate__animated animate__wobble animate__delay-1s"></i> Soy <span>Axel Brian Dip</span>,</p>
                 <h1 className='title'>DESARROLLADOR <br/> FRONT-END.</h1>
-                <p className='description'>Me dedico al diseño, creación, realización y mantenimiento <br /> de páginas y/o aplicaciones web.</p>
+                <p className='description'>Me dedico al diseño, creación, realización y mantenimiento de páginas y/o aplicaciones web.</p>
                 <button className='btn-contactame'>Contactame</button>
-            </div>
+            </article>
+            <article className="footer-home col-12">
+                <p>¿Te cuento un poco más sobre mi?</p>
+                <i className="fa-solid fa-circle-arrow-down"></i>
+            </article>
         </section>
     );
 };
