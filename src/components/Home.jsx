a8e449b04d1afee052d3deaf51f2536d549a3c4b
axelbriandip <axelbriandip.rg@gmail.com>
2022-08-18 03:03:51 -0300

    text in home done II - ready

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,29 +4,29 @@ const Home = () => {
     return (
         <section className='container-home'>
             {/* circles */}
-            <div className="circles">
+            <div className="circles col-5 col-s-12">
                 <div className="photo animate__animated animate__backInDown"></div>
                 <div className="social-media animate__animated animate__backInLeft">
                     <div className="item-sm linkedin" id='tooltip'>
                         <i className="fa-brands fa-linkedin-in"></i>
-                        <span id='tooltipText'>Linkedin</span>
+                        <span id='tooltipText'>Visitar Linkedin</span>
                     </div>
                     <div className="item-sm cv" id='tooltip'>
                         <i className="fa-solid fa-file"></i>
-                        <span id='tooltipText'>Curriculum Vitae</span>
+                        <span id='tooltipText'>Descargar CV</span>
                     </div>
                     <div className="item-sm github" id='tooltip'>
                         <i className="fa-brands fa-github"></i>
-                        <span id='tooltipText'>Github</span>
+                        <span id='tooltipText'>Visitar Github</span>
                     </div>
                 </div>
             </div>
             {/* text */}
-            <div className="text animate__animated animate__backInRight">
+            <div className="text col-5 col-s-12 animate__animated animate__backInRight">
                 <p className='name'><i className="fa-solid fa-hand animate__animated animate__wobble animate__delay-1s"></i> Soy <span>Axel Brian Dip</span>,</p>
-                <h1 className='title'>DESARROLLADOR <br /> FRONT-END.</h1>
+                <h1 className='title'>DESARROLLADOR <br/> FRONT-END.</h1>
                 <p className='description'>Me dedico al diseño, creación, realización y mantenimiento <br /> de páginas y/o aplicaciones web.</p>
-                <button>Contactame</button>
+                <button className='btn-contactame'>Contactame</button>
             </div>
         </section>
     );
