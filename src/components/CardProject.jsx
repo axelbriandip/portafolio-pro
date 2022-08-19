c9b254473683c7128dfc7da1f4a51b89328e178a
axelbriandip <axelbriandip.rg@gmail.com>
2022-08-19 20:29:24 -0300

    images in projects added

--- src/components/CardProject.jsx
+++ src/components/CardProject.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const CardProject = ({ photo, title , description}) => {
+const CardProject = ({ photo, title , description, link_github, link_netlify}) => {
     return (
         <div className="card-project">
             <img src={photo} alt="Avatar" className="image"/>
@@ -10,8 +10,8 @@ const CardProject = ({ photo, title , description}) => {
                     <p>{description}</p>
                     <br/>
                     <div className="links-project">
-                        <a href="#" className='github-project'>Ver Github</a>
-                        <a href="#">Ver Netlify</a>
+                        <a href={link_github} target='_blank' className='github-project'>Ver Github</a>
+                        <a href={link_netlify} target='_blank'>Ver Netlify</a>
                     </div>
                 </div>
             </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 // import img
-import photo from '../img/portada-pokemon.jpg';
+import pokemon from '../img/pokemon1-01-01.jpg';
+import rym from '../img/rym1-01-01.jpg';
+import ecommerce from '../img/ecommerce1-01-01.jpg';
 
 import CardProject from './CardProject';
 
@@ -11,19 +13,25 @@ const Projects = () => {
             <h2>Algunos proyectos..</h2>
             <div className="container-cards-projects">
                 <CardProject
-                photo={photo}
+                photo={pokemon}
                 title={'Pokedex'}
                 description={'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error, maiores corporis! Architecto optio placeat accusantium eaque sit porro. Illum quia aliquid.'}
+                link_github={'https://github.com/axelbriandip/pokedex'}
+                link_netlify={'https://pokedex-axelbriandip.netlify.app'}
                 />
                 <CardProject
-                photo={photo}
+                photo={rym}
                 title={'Rick & Morty'}
                 description={'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error, maiores corporis! Architecto optio placeat accusantium eaque sit porro. Illum quia aliquid.'}
+                link_github={'https://github.com/axelbriandip/rickAndMorty'}
+                link_netlify={'https://rickandmorty-academlo.netlify.app'}
                 />
                 <CardProject
-                photo={photo}
+                photo={ecommerce}
                 title={'Ecommerce'}
                 description={'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error, maiores corporis! Architecto optio placeat accusantium eaque sit porro. Illum quia aliquid.'}
+                link_github={'https://github.com/axelbriandip/ecommerce'}
+                link_netlify={'https://ecommerce-axelbriandip.netlify.app'}
                 />
             </div>
         </div>
